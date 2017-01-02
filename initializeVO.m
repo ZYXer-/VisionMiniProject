372ce99955100cb84b5ca0d4fa08e43974438eea
jmsos <etherealstrife@gmail.com>
2017-01-02 16:44:55 +0100

    copied keypoint functions from solutions, incorporating into initializeVO.m

--- initializeVO.m
+++ initializeVO.m
@@ -3,5 +3,21 @@ function [ cameraPose, initialState ] = initializeVO( initialFrame, secondFrame 
 %   Detailed explanation goes here
 
 
+%% Initial image processing
+% Select keypoints
+keypoints = selectKeypoints(...
+    harris_scores, num_keypoints, nonmaximum_supression_radius);
+
+% Describe keypoints and show 16 strongest keypoint descriptors
+descriptors = describeKeypoints(img, keypoints, descriptor_radius);
+
+%% Match descriptors between first two images
+
+harris_scores_2 = harris(img_2, harris_patch_size, harris_kappa);
+keypoints_2 = selectKeypoints(...
+    harris_scores_2, num_keypoints, nonmaximum_supression_radius);
+descriptors_2 = describeKeypoints(img_2, keypoints_2, descriptor_radius);
+
+matches = matchDescriptors(descriptors_2, descriptors, match_lambda);
 end
 
