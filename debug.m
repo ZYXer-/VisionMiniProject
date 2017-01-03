5ba10d59759c3c0abf1425efdbddfa51bcf45279
Henry Raymond <github@henryraymond.com>
2017-01-03 17:41:08 +0100

    Tuesday's work

--- debug.m
+++ debug.m
@@ -1,17 +1,29 @@
-img = imread('kitti/00/image_0/000000.png');
+%img = imread('kitti/00/image_0/000000.png');
 
-harris = getHarrisScores(double(img), 9, 0.08);
+%harris = getHarrisScores(double(img), 9, 0.08);
 
-keypoints = selectKeypoints(harris, 200, 9);
+%keypoints = selectKeypoints(harris, 200, 9);
 
-figure(1);
-imagesc(mat2gray(harris));
-axis equal;
-axis off;
-hold on;
-plot(keypoints(2, :), keypoints(1, :), 'rx', 'Linewidth', 2);
+%descriptors = getDescriptors(img, keypoints, 9);
+
+%figure(1);
+%imagesc(mat2gray(harris));
+%axis equal;
+%axis off;
+%hold on;
+%plot(keypoints(2, :), keypoints(1, :), 'rx', 'Linewidth', 2);
+
+%for i = 1:16
+   % subplot(4, 4, i);
+    %patch_size = 2 * 9 + 1;
+    %imagesc(uint8(reshape(descriptors(:,i), [patch_size patch_size])));
+    %axis equal;
+    %axis off;
+%end
 
 
+initialFrame = imread('kitti/00/image_0/000000.png');
+secondFrame = imread('kitti/00/image_0/000002.png');
 
-pause;
 
+[ transformWorld2Camera, initialState ] = initializeVO( initialFrame, secondFrame );
--- getHarrisScores.m
+++ getHarrisScores.m
@@ -1,12 +1,15 @@
-function [ harrisScores ] = getHarrisScores( image, patchSize, traceWeight )
+function [ harrisScores ] = getHarrisScores( frame, patchSize, traceWeight )
+
+    % convert frame to double
+    frame = double(frame);
 
     % sobel filters
     sobelX = [ -1 0 1; -2 0 2; -1 0 1 ]; 
     sobelY = sobelX.'; 
     
     % image derivates
-    dX = conv2(image, sobelX, 'valid');
-    dY = conv2(image, sobelY, 'valid');
+    dX = conv2(frame, sobelX, 'valid');
+    dY = conv2(frame, sobelY, 'valid');
     
     % covert to double
     dX = double(dX);
@@ -29,7 +32,7 @@ function [ harrisScores ] = getHarrisScores( image, patchSize, traceWeight )
     harrisScores = determinant - (traceWeight * (trace .* trace));
     
     % pad scores so that there is a score for each pixel
-    imageSize = size(image);
+    imageSize = size(frame);
     scoresSize = size(harrisScores);
     padVertical = floor((imageSize(1) - scoresSize(1)) / 2.0);
     padHorizontal = floor((imageSize(2) - scoresSize(2)) / 2.0);
