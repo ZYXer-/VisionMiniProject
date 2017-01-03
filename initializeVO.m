8542c8435bf603fc8c8bc9f368868eb9e9475901
jmsos <etherealstrife@gmail.com>
2017-01-03 02:40:16 +0100

    started adding triangulation, added tasks for initialization and processing phases

--- initializeVO.m
+++ initializeVO.m
@@ -14,9 +14,9 @@ match_lambda = 4;
 
 %% Initial image processing
 
-%Calculate Harris scores
-harris_scores = harris(initialFrame, harris_patch_size, harris_kappa);
-assert(min(size(harris_scores) == size(img)));
+% Calculate Harris scores
+harris_scores = getHarrisScores(initialFrame, harris_patch_size, harris_kappa);
+assert(min(size(harris_scores) == size(initialFrame)));
 
 % Select keypoints
 keypoints = selectKeypoints(...
@@ -27,11 +27,22 @@ descriptors = describeKeypoints(img, keypoints, descriptor_radius);
 
 %% Match descriptors between first two images
 
-harris_scores_2 = harris(secondFrame, harris_patch_size, harris_kappa);
+harris_scores_2 = getHarrisScores(secondFrame, harris_patch_size, harris_kappa);
 keypoints_2 = selectKeypoints(...
     harris_scores_2, num_keypoints, nonmaximum_supression_radius);
 descriptors_2 = describeKeypoints(secondFrame, keypoints_2, descriptor_radius);
 
 matches = matchDescriptors(descriptors_2, descriptors, match_lambda);
+
+%% Stereo matching to establish correspondences 
+
+%% Remove outliers
+% is this really necessary for initialization, or can we get by with just using
+% the "best" matches? 
+
+%% Triangulate the keypoint correspondences (2D x 2 -> 3D)
+%exercise 4 part 4
+%exercise 5 2-view geometry
+
 end
 
--- processFrame.m
+++ processFrame.m
@@ -1,6 +1,26 @@
-function [ output_args ] = processFrame( input_args )
-%PROCESSFRAME Summary of this function goes here
-%   Detailed explanation goes here
+function [ newCameraPose, newState ] = processFrame( prevCameraPose, newFrame, prevState )
+%PROCESSFRAME 
+%   should we save the old camera pose before every re-iteration (in main?)? (only takes
+%   in initial from initializeVO)
+
+
+% what do we want to "pass forward"? Just the pixel coordinates on
+% prevImage, or the full descriptor sets? 
+
+%% Match descriptors between current image and previous image
+
+%% Stereo matching to establish correspondences 
+% ? is this necessary? 
+
+%% Remove outliers
+% RANSAC
+
+%% Triangulate new keypoint correspondences (2D x 2 -> 3D)
+%exercise 4 part 4
+%exercise 5 2-view geometry
+
+%% Estimate camera pose
+% PNP - lecture 3
 
 
 end
